8925d1ea264407ba946dfafbdf6c0ba620021635
Vinicius <viniciusantos.florencio@gmail.com>
2021-12-18 16:16:39 -0300

    feat:file for server

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express from 'express'
+import sequelize from './database'
+import dotenv from 'dotenv'
+import UserRouter from './routes/User'
+
+dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 3000
+
+sequelize.sync()
+app.use(express.json())
+
+app.use('/api', UserRouter)
+
+app.use((req, res, next) => {
+  res.status(404).send()
+})
+
+app.use((req, res, next) => {
+  res.status(500).send()
+})
+
+app.listen(PORT, () => {
+  console.log(`App is running on port http://localhost:${PORT}`)
+})
