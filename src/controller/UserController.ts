1ef53e4382caec265244337a85c7771594a77838
Vinicius <viniciusantos.florencio@gmail.com>
2021-12-19 11:55:25 -0300

    feat:user controller instaciated user service

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,32 +1,30 @@
 import { Request, Response } from 'express'
+import UserServices from '../services/UserService'
 
-import UserModel from '../Models/User'
 import User from '../global/classes/User'
 import Functions from '../global/functions'
 
 class UserController {
-  async create(req: Request, res: Response) {
+  constructor(private service: UserServices) {
+    this.create = this.create.bind(this)
+  }
+
+  create(req: Request, res: Response) {
     if (Functions.hasUnfilledField(req.body)) {
       res.status(400).send({ error: 'Bad Request' })
     }
+
     const { name, email, password, address, cpf, occupation } = req.body
-    const user = new User({ name, email, password, address, cpf, occupation })
+    const requestUser = new User({ name, email, password, address, cpf, occupation })
+    console.log(this.service)
 
-    try {
-      const dbUser = await UserModel.create({
-        name: user.getName(),
-        email: user.getEmail(),
-        password: user.getPassword(),
-        address: user.getAdress(),
-        cpf: user.getCpf(),
-        occupation: user.getOcuppation(),
+    this.service
+      .create(requestUser)
+      .then(user => res.status(201).send({ user }))
+      .catch(error => {
+        res.status(500).send({ error })
       })
-
-      res.status(201).send({ user: dbUser })
-    } catch (error) {
-      res.status(500).send({ error: error })
-    }
   }
 }
 
-export default new UserController()
+export default new UserController(new UserServices())
