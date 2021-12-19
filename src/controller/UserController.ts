6a4494d37f86711894fca5364d6a86b39505ea74
Vinicius <viniciusantos.florencio@gmail.com>
2021-12-19 11:09:37 -0300

    feat: User for create a new user in db

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express'
 
 import UserModel from '../Models/User'
+import User from '../global/classes/User'
 import Functions from '../global/functions'
 
 class UserController {
@@ -9,18 +10,19 @@ class UserController {
       res.status(400).send({ error: 'Bad Request' })
     }
     const { name, email, password, address, cpf, occupation } = req.body
+    const user = new User({ name, email, password, address, cpf, occupation })
 
     try {
-      const user = await UserModel.create({
-        name,
-        email,
-        password,
-        address,
-        cpf,
-        occupation,
+      const dbUser = await UserModel.create({
+        name: user.getName(),
+        email: user.getEmail(),
+        password: user.getPassword(),
+        address: user.getAdress(),
+        cpf: user.getCpf(),
+        occupation: user.getOcuppation(),
       })
 
-      res.status(201).send({ user })
+      res.status(201).send({ user: dbUser })
     } catch (error) {
       res.status(500).send({ error: error })
     }
