d02629936d17c8e50459c92d4b1c588fc8753fcc
Vinicius <viniciusantos.florencio@gmail.com>
2021-12-19 06:26:25 -0300

    feat:retuning user before create

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -4,15 +4,14 @@ import UserModel from '../Models/User'
 import hasUnfilledField from '../global/functions/hasUnFilledField'
 
 class UserController {
-  create = async (req: Request, res: Response) => {
+  async create(req: Request, res: Response) {
     if (hasUnfilledField(req.body)) {
       res.status(400).send({ error: 'Bad Request' })
     }
 
     const { name, email, password, address, cpf, occupation } = req.body
-
     try {
-      await UserModel.create({
+      const user = await UserModel.create({
         name,
         email,
         password,
@@ -20,6 +19,8 @@ class UserController {
         cpf,
         occupation,
       })
+
+      res.status(201).send({ user })
     } catch (error) {
       res.status(500).send({ error: error })
     }
