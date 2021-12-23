e1214690fdbeb2d5eacba6da3b35274999eced3e
Vinicius <viniciusantos.florencio@gmail.com>
2021-12-23 18:42:50 -0300

    feat:abstract functions for hash and compare password

--- src/global/Abstract/index.ts
+++ src/global/Abstract/index.ts
@@ -1,3 +1,5 @@
 export default abstract class Utils {
   abstract hasUnfilledField(body: { [field: string]: any }): boolean
+  abstract hashPassword(userPassword: string, saltRounds: number): Promise<string>
+  abstract comparePassword(password: string, hashedPassword: string): Promise<boolean>
 }
