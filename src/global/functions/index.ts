4202305223aabf24c3f17dcc0da8b1676cf182b7
Vinicius <viniciusantos.florencio@gmail.com>
2021-12-23 18:43:07 -0300

    feat:global function for hash password

--- src/global/functions/index.ts
+++ src/global/functions/index.ts
@@ -1,6 +1,7 @@
 import Utils from '../Abstract'
-
+import bcrypt from 'bcrypt'
 class Functions implements Utils {
+  private saltRounds = 10
   hasUnfilledField(body: { [field: string]: any }): boolean {
     for (const field in body) {
       if (!body[field]) {
@@ -8,6 +9,16 @@ class Functions implements Utils {
       }
     }
     return false
+  }
+
+  async hashPassword(userPassword: string): Promise<string> {
+    const encryptedPassword = await bcrypt.hash(userPassword, this.saltRounds)
+    return encryptedPassword
+  }
+
+  async comparePassword(password: string, hashedPassword: string): Promise<boolean> {
+    const result = await bcrypt.compare(password, hashedPassword)
+    return result
   }
 }
 
